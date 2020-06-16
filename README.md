609ae03882e7944044d233406b3f2d71f8f0561e
DevMo-13 <moniquecinatl@gmail.com>
2020-06-16 11:29:25 -0700

    All functionality complete

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Project 6 - Static Node.js and Express Site
  Treehouse FSJS Techdegree Project 6 - Static Node.js and Express Site
 
-This project creates a portfolio site to showcase the projects I've built so far. The site will contain a modern landing page, an about page with my contact info, and a series of project pages to show off and detail my first 5 projects from the Treehouse FSJS techdegree.
+This project utilizes Node.js, Express, and Pug to create a portfolio site to showcase the projects I've built so far. The site will contain a modern landing page, an about page with my contact info, and a series of project pages to show off and detail my first 5 projects from the Treehouse FSJS techdegree.
 
 ---
 
--- app.js
+++ app.js
@@ -18,8 +18,7 @@ app.use('/static', express.static('public'));
 // Index route renders the home page with the locals set to data.projects.
 app.get('/', (req, res) => {
     const projects = data.projects;
-    const templateData = {projects};
-    res.render('index', templateData);
+    res.render('index', {projects});
 });
 
 // About route renders the about page.
@@ -27,29 +26,29 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-// Project routes (based on the id of the project) renders customized pug templates to show off each project.
+// Project route (based on the id of the project) renders a customized project pug template to show off the selected project.
 app.get('/project/:id', (req, res, next) => {
     const id = req.params.id;
     const thisProjData = data.projects[id];
-    const templateData = {thisProjData};
-    res.render('project', templateData);
+    res.render('project', {thisProjData});
 });
 
-// Error handlers
+// Catches a 404 error to send to error handler.
 app.use((req, res, next) => {
-    const err = new Error(`Oops, this not the page you're looking for!`);
+    const err = new Error(`Oops, page not found.`);
     err.status = 404;
     console.log('Sorry, this page does not exist.');
     next(err);
 });
 
+// Error handler that renders the error pug template.
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
     res.render('error');
 });
 
-// Runs the app on local host at port 3000
+// Runs the app on local host at port 3000.
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000.');
 });
